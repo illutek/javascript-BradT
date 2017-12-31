b340781ccfc924540f32ca2fe5e8a62dcf098f45
Vandenborne Stefan <stefan@illutek.be>
2017-12-31 22:13:54 +0100

    end section 2-6

--- js/app.js
+++ js/app.js
@@ -1,3 +1,37 @@
 // Log to console
 
-console.log('Hello World');
+// var name = 'John Doe';
+// console.log(name);
+// name = "Peter Smith";
+// console.log(name);
+
+// // Init var
+// var greeting;
+// console.log(greeting);
+// greeting = "Hello World";
+// console.log(greeting);
+
+// let name = 'John Doe';
+// console.log(name);
+// name = "Peter Smith";
+// console.log(name);
+
+// Const
+const name = 'John Doe';
+console.log(name);
+
+// Can not reassign
+// name = "Peter Smith";
+// console.log(name);
+
+const person = {
+  name: 'John',
+  age: 30
+}
+
+person.name = 'Sara';
+console.log(person);
+
+const numbers = [1,2,3,4,5];
+numbers.push(6);
+console.log(numbers);
